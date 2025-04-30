175dbc99dc25985a53fbb3c6e852645af8f7ef92
ampop-2025 <josineiamaral@gmail.com>
2025-04-30 19:00:41 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,6 @@
+// firebase-config.js
+
+// Configuração da sua conta Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDJ6N3zQkTzyiMR27pfvE-iE1qCE_5zavg",
   authDomain: "ampop-4c96f.firebaseapp.com",
@@ -7,9 +10,5 @@ const firebaseConfig = {
   appId: "1:1008863242618:web:caab3844b7af163d0a0ec3"
 };
 
+// Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"></script>
-<script src="firebase-config.js"></script>
