8dc9f627fb30bf24effd5e57267f5f495b6ae0b0
ampop-2025 <josineiamaral@gmail.com>
2025-04-30 18:39:46 -0300

    Update and rename index to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ6N3zQkTzyiMR27pfvE-iE1qCE_5zavg",
+  authDomain: "ampop-4c96f.firebaseapp.com",
+  projectId: "ampop-4c96f",
+  storageBucket: "ampop-4c96f.appspot.com",
+  messagingSenderId: "1008863242618",
+  appId: "1:1008863242618:web:caab3844b7af163d0a0ec3"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"></script>
+<script src="firebase-config.js"></script>
