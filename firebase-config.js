bd94082a93e4e3b0a191c24f53ec7fb16303c1b3
ampop-2025 <josineiamaral@gmail.com>
2025-05-06 09:26:48 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJ6N3zQkTzyiMR27pfvE-iE1qCE_5zavg",
+  authDomain: "ampop-4c96f.firebaseapp.com",
+  projectId: "ampop-4c96f",
+  storageBucket: "ampop-4c96f.firebasestorage.app",
+  messagingSenderId: "1008863242618",
+  appId: "1:1008863242618:web:caab3844b7af163d0a0ec3"
+};
+
+export const ADMIN_UID = "QBRIuIdqwEb7a5NKO8NKFRsGuW42";
